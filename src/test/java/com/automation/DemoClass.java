5950afa40c7a0774dff1e255d7a00c7e80c462d6
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-07 17:35:19 +0300

    Initial commit

---
+++ src/test/java/com/automation/DemoClass.java
@@ -0,0 +1,15 @@
+package com.automation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.instagram.com/tima_2948/");
+
+
+    }
+}
